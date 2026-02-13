8783809f21e4736b812eeb2ae2b46c36b74b1fde
emilyzuniga049 <emilyzuniga049@gmail.com>
2026-02-12 17:49:06 -1200

    Agrego workshop 2

---
+++ Workshop2/server/index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const Course = require('./models/course');
+
+mongoose.connect('mongodb://127.0.0.1:27017/utnapi');
+const database = mongoose.connection;
+
+
+database.on('error', (error) => {
+    console.log(error)
+});
+
+database.once('connected', () => {
+    console.log('Database Connected');
+});
+
+
+const app = express();
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors({
+  domains: '*',
+  methods: '*'
+}));
+
+
+//routes
+app.post('/course', async (req, res) => {
+    const course = new Course({
+        name: req.body.name,
+        credits: req.body.credits
+    })
+
+    try {
+        const courseCreated = await course.save();
+        //add header location to the response
+        res.header('Location', `/course?id=${courseCreated._id}`);
+        res.status(201).json(courseCreated)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+});
+
+app.get('/course', async (req, res) => {
+    try{
+        //if id is passed as query param, return single course else return all courses
+        if(!req.query.id){
+            const data = await Course.find();
+            return res.status(200).json(data)
+        }
+        const data = await Course.findById(req.query.id);
+        res.status(200).json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.put('/course/:id', async (req, res) => {
+  try {
+    const updatedCourse = await Course.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    )
+
+    if (!updatedCourse) {
+      return res.status(404).json({ message: 'Course not found' })
+    }
+
+    res.json(updatedCourse)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+app.delete('/course/:id', async (req, res) => {
+  try {
+    const deletedCourse = await Course.findByIdAndDelete(req.params.id)
+
+    if (!deletedCourse) {
+      return res.status(404).json({ message: 'Course not found' })
+    }
+
+    res.json({ message: 'Course deleted successfully' })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+
+
+//start the app
+app.listen(3001, () => console.log(`UTN API service listening on port 3001!`))
